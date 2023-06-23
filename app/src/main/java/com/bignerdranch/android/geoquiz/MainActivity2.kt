b59c9cdcac5a275741e67d2632e6e1d71dd09449
Drimuar <Drimuar@gmail.com>
2023-06-24 00:43:43 +0700

    add repeat button

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity2.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity2.kt
@@ -1,11 +1,15 @@
 package com.bignerdranch.android.geoquiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var repeatButton: Button
     private lateinit var textView1: TextView
     private val quizViewModel: QuizViewModel by lazy {
         ViewModelProviders.of(this)[QuizViewModel::class.java]
@@ -13,9 +17,14 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        repeatButton = findViewById(R.id.repeat_button)
         val data = intent.extras!!.getInt("testNameData")
         findViewById<TextView>(R.id.text_view).apply {
             text = data.toString()
+        }
+        repeatButton.setOnClickListener { view: View ->
+            val ma1intent = Intent(this@MainActivity2, MainActivity::class.java)
+            startActivity(ma1intent)
         }
     }
 }
